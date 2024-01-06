5c6c1b08259129d0d231094efbc476cbffe14b0d
Vinicius <vini383@gmail.com>
2024-01-06 12:58:03 -0300

    first commit

---
+++ Integer_Ratio/Program.cs
@@ -0,0 +1,42 @@
+﻿class Result
+{
+
+    /*
+     * Complete the 'plusMinus' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void plusMinus(List<int> arr)
+    {
+        int total = arr.Count;
+        int positiveCount = 0, negativeCount = 0, zeroCount = 0;
+
+        foreach (int num in arr)
+        {
+            if (num > 0)
+                positiveCount++;
+            else if (num < 0)
+                negativeCount++;
+            else
+                zeroCount++;
+        }
+
+        Console.WriteLine(((double)positiveCount / total).ToString("F6"));
+        Console.WriteLine(((double)negativeCount / total).ToString("F6"));
+        Console.WriteLine(((double)zeroCount / total).ToString("F6"));
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        Result.plusMinus(arr);
+    }
+}
