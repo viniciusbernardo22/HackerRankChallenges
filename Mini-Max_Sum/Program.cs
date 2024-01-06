f72fccef87e500c65426d6e5840c47ed8288a55a
Vinicius <vini383@gmail.com>
2024-01-06 16:21:46 -0300

    feat: added Mini-Max_Sum Challenge

---
+++ Mini-Max_Sum/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'miniMaxSum' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void miniMaxSum(List<int> arr)
+    {
+        var lowestAmount = arr.Select(num => (long)num).OrderBy(num => num).Take(4).Sum();
+        var highestAmount = arr.Select(num => (long)num).OrderByDescending(num => num).Take(4).Sum();
+
+        Console.WriteLine($"{lowestAmount} {highestAmount}");
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        Result.miniMaxSum(arr);
+    }
+}
